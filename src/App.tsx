ade1ffd056810f7860b17c5559eecce58ea74b39
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-28 12:20:26 +0200

    Added bonus

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Route, Routes} from "react-router-dom";
 import CharacterSearch from "./components/CharacterSearch.tsx";
 import Header from "./components/Header.tsx";
+import CharacterDetail from "./components/CharacterDetail.tsx";
 
 export default function App() {
     return (
@@ -10,6 +11,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<p>Hello Visitor!</p>}/>
                 <Route path="/characters" element={<CharacterSearch/>}/>
+                <Route path="/characters/:id" element={<CharacterDetail/>}/>
             </Routes>
         </>
     )
