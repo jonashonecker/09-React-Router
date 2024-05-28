3a3585ae31f8c2376835f493703498467d757b03
Jonas Honecker <honeckerjonas@gmail.com>
2024-05-28 11:39:26 +0200

    Solved challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import './App.css'
-import CharacterGallery from "./components/CharacterGallery.tsx";
-import {useState} from "react";
-import {characters} from "./Characters.ts";
+import {Route, Routes} from "react-router-dom";
+import CharacterSearch from "./components/CharacterSearch.tsx";
+import Header from "./components/Header.tsx";
 
 export default function App() {
-    const [searchText, setSearchText] = useState("");
-
-    const filteredCharacters = characters
-        .filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()));
-
     return (
         <>
-            <input type="text" onChange={(e) => setSearchText(e.target.value)} placeholder="Search for a character"/>
-            {
-                filteredCharacters.length > 0
-                    ? <CharacterGallery characters={filteredCharacters}/>
-                    : <p>No characters found</p>
-            }
+            <Header/>
+            <Routes>
+                <Route path="/" element={<p>Hello Visitor!</p>}/>
+                <Route path="/characters" element={<CharacterSearch/>}/>
+            </Routes>
         </>
-    );
+    )
 }
